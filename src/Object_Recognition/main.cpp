5a3c4729f8d0205374136d2ffe53850eaa1b72fd
Jean-Baptiste Weibel <jb.weibel@gmail.com>
2017-02-08 01:55:31 +0100

    Updated architecture

--- src/Object_Recognition/main.cpp
+++ src/Object_Recognition/main.cpp
@@ -12,119 +12,96 @@ using namespace yarp::sig;
 
 typedef std::pair<string, float> Prediction;
 
-class Processing : public BufferedPort<ImageOf<PixelRgb> >
+
+class ImageProcessor : public PortReader
 {
 protected:
-  bool init_;
-  std::string moduleName;
-  BufferedPort<Bottle> predictionsOut;
   Classifier *classifier_;
 
-public:
-
-  Processing(const std::string &moduleName,const  std::string &model_path, const std::string &weights_path, const std::string &mean_file, const std::string &label_file)
+  bool read(ConnectionReader &connection)
   {
-    this->moduleName = moduleName;
-    classifier_ = new Classifier(model_path, weights_path, mean_file, label_file);
-  }
+    yInfo() << "About to recognize an image";
+    ImageOf<PixelRgb> imageIn;
+    if (!imageIn.read(connection))
+        return false;
 
-  ~Processing()
-  {
-    delete classifier_;
-  }
+    cv::Mat in_cv = cv::cvarrToMat( (IplImage *)imageIn.getIplImage() );
+    std::vector<Prediction> predictions = classifier_->Classify(in_cv, 7);
 
-  bool open()
-  {
-    this->useCallback();
+    for (size_t i=0; i < predictions.size(); i++) {
+      yInfo() << "Class " << predictions[i].first
+              << " predicted with confidence " << predictions[i].second;
+    }
 
-    BufferedPort<ImageOf<PixelRgb> >::open("/" + moduleName + "/img_stream");
-    predictionsOut.open("/" + moduleName + "/output_class");
+    Bottle reply; 
+    if (predictions.size() > 0)
+    {
+      // TODO What kind of output ? just one predictions or multiple, with or without confidence ?
+      reply.addString(predictions[0].first);
+    } else
+    {
+      reply.addString("NO_PREDICTION");
+    }
+
+    if (ConnectionWriter *writer=connection.getWriter())
+      reply.write(*writer);
 
     return true;
   }
 
-  void close()
+public:
+  ImageProcessor(const  std::string &model_path, const std::string &weights_path, const std::string &mean_file, const std::string &label_file)
   {
-    predictionsOut.close();
-    BufferedPort<ImageOf<PixelRgb> >::close();
+    classifier_ = new Classifier(model_path, weights_path, mean_file, label_file);
   }
 
-  void interrupt()
+  ~ImageProcessor()
   {
-    BufferedPort<ImageOf<PixelRgb> >::interrupt();
-  }
-
-  void onRead(ImageOf<PixelRgb> &img) {
-    cv::Mat in_cv = cv::cvarrToMat( (IplImage *)img.getIplImage() );
-    std::vector<Prediction> predictions = classifier_->Classify(in_cv, 3);
-
-    for (int i=0; i < predictions.size(); i++) {
-      std::cout << "Class " << predictions[i].first
-                << " predicted with confidence " << predictions[i].first
-                << std::endl;
-    }
-
-    Bottle &outPreds = predictionsOut.prepare();
-
-    if (predictions.size() > 0)
-    {
-      // TODO What kind of output ? just one predictions or multiple, with or without confidence ?
-      outPreds.addString(predictions[0].first);
-    } else
-    {
-      outPreds.addString("NO_PREDICTION");
-    }
-    predictionsOut.write();
+    delete classifier_;
   }
 };
 
-
 class RecoModule: public RFModule
 {
-
 protected:
   ResourceFinder *rf;
   RpcServer rpcPort;
+  RpcServer testRpcPort;
+  ImageProcessor *imgProc;
   bool closing;
-
-  Processing *processing;
-  friend class processing;
-
-  /*bool attach(RpcServer &source)
-  {
-    return this->yarp().attachAsServer(source);
-  }*/
 
 public:
   bool configure(ResourceFinder &rf)
   {
     closing = false;
     this->rf = &rf;
-    std::string moduleName = rf.check("name", Value("Objet_Recognition")).asString();
+    std::string moduleName = rf.check("name", Value("Object_Recognition")).asString();
     std::string model_path = rf.check("model_path", Value("data/deploy.prototxt")).asString();
-    std::string weights_path = rf.check("weights_path", Value("data/bvlc_reference_caffenet.caffemodel")).asString();
-    std::string mean_file = rf.check("mean_file", Value("data/imagenet_mean.binaryproto")).asString();
-    std::string label_file = rf.check("label_file", Value("data/synset_words.txt")).asString();
+    std::string weights_path = rf.check("weights_path", Value("data/garbage_fine_tuning_iter_15000.caffemodel")).asString();
+    std::string mean_file = rf.check("mean_file", Value("data/mean.binaryproto")).asString();
+    std::string label_file = rf.check("label_file", Value("data/labels.txt")).asString();
 
-    rpcPort.open(("/" + moduleName + "/rpc").c_str());
-    processing = new Processing(moduleName, model_path, weights_path, mean_file, label_file);
-    processing->open();
+    imgProc = new ImageProcessor(model_path, weights_path, mean_file, label_file);
+    rpcPort.setReader(*imgProc);
 
-    attach(rpcPort);
+    rpcPort.open(("/" + moduleName + "/image").c_str());
+    testRpcPort.open(("/" + moduleName + "/rpc").c_str());
+
+    attach(testRpcPort);
 
     return true;
   }
 
   bool interruptModule()
   {
+    rpcPort.interrupt();
     return true;
   }
 
   bool close()
   {
-    processing->interrupt();
-    processing->close();
-    delete processing;
+    rpcPort.close();
+    delete imgProc;
     return true;
   }
 
@@ -139,42 +116,11 @@ public:
     return true;
   }
 
-  bool respond(const Bottle &command, Bottle &reply)
-  {
-    string cmd = command.get(0).asString();
-
-    if (cmd == "help")
-    {
-      reply.addVocab(Vocab::encode("many"));
-      reply.addString("recognize_file");
-      reply.addString("quit");
-    }
-    else if (cmd == "recognize_file")
-    {
-      string filename = command.get(1).asString();
-      cv::Mat img_cv = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
-
-      yarp::sig::ImageOf<yarp::sig::PixelRgb> &inImage  = processing->prepare();
-
-      IplImage img = img_cv;
-      inImage.resize(img.width, img.height);
-      cvCopy( &img, (IplImage *) inImage.getIplImage());
-      processing->write();
-    }
-    else
-    {
-      return RFModule::respond(command, reply);
-    }
-
-    return true;
-  }
-
   double getPeriod()
   {
     return 0.0; //sync upon incoming images
   }
 };
-
 
 int main(int argc, char** argv)
 {
